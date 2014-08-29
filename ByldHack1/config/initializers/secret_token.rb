3b65bb0a536165768bb1179f27d25d72e9b0013c
Ambar Pal <palambar.96@gmail.com>
2014-08-29 22:37:50 +0530

    All Added

---
+++ ByldHack1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project::Application.config.secret_key_base = 'f6005dc705127accea8475a8d473a3bee63522ce8d1bd90bf2b7b87f2076da3d083e8348cfdf6bca5ba764d7bba2c2db16ba484fd15ca129478979776e15ec52'
